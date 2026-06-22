6dc3d3a7d15f6cbece40b88d1ad2515589b26283
210TauHu <vonguyenhagiang7@gmail.com>
2026-06-22 20:01:08 +0700

    Update Test2Hub.lua

--- Test2Hub.lua
+++ Test2Hub.lua
@@ -1,9 +1,1 @@
-while true do
-	wait(0.25)
-
-	for _, player in pairs(game.Players:GetPlayers()) do
-		if player.Character and player.Character:FindFirstChild("Humanoid") then
-			player.Character.Humanoid.Health = 0
-		end
-	end
 end
